20e2e6c1da28849bf7567e38e0db065a50202324
CoderChenJun <Coder.ChenJun@qq.com>
2018-03-29 09:20:33 +0800

    Initial Commit

---
+++ TestTabBarController/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestTabBarController
+//
+//  Created by CoderChenJun on 2018/3/29.
+//  Copyright © 2018年 奥奈斯特（宁波）软件技术有限公司. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
